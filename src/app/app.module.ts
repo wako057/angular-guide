b311f7bb43a01d8de2b6c087fccc412634e29df8
Gregs <gregory.gerard@wister.fr>
2017-02-27 05:48:21 +0100

    Menage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ClickMeComponent } from './click-me.component';
-import { KeyUpComponent_V1 } from './keyupv1.component';
-import { LoopbackComponent } from './loop-back.component';
-import { KeyUpComponent_V2 } from './keyupv2.component';
-import { KeyUpComponent_V3 } from './keyupv3.component';
-import { KeyUpComponent_V4 } from './keyupv4.component'
-import { LittleTourComponent } from './little-tour.component'
+import { ClickMeComponent } from './7.UserInput/click-me.component';
+import { KeyUpComponent_V1 } from './7.UserInput/keyupv1.component';
+import { LoopbackComponent } from './7.UserInput/loop-back.component';
+import { KeyUpComponent_V2 } from './7.UserInput/keyupv2.component';
+import { KeyUpComponent_V3 } from './7.UserInput/keyupv3.component';
+import { KeyUpComponent_V4 } from './7.UserInput/keyupv4.component'
+import { LittleTourComponent } from './7.UserInput/little-tour.component'
 @NgModule({
   declarations: [
     AppComponent, ClickMeComponent, KeyUpComponent_V1, LoopbackComponent, KeyUpComponent_V2, KeyUpComponent_V3,
