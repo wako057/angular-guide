ea393db4c5db44a31896328f6f7d004a40525392
Gregs <gregory.gerard@wister.fr>
2017-03-03 18:54:20 +0100

    Chapitre 10 Binding target - One-time string initialization - Property binding or interpolation

--- src/app/10.TemplateSyntax/chapter10.component.ts
+++ src/app/10.TemplateSyntax/chapter10.component.ts
@@ -15,11 +15,18 @@ export class Chapter10Component {
     public title = '';
     private config = null ;
     public heroes = null;
+    public heroImageUrl = '';
+    private evilTitle;
+
 
     constructor(@Inject(APP_CONFIG) config: AppConfig, public logger: Logger, heroService: HeroService) {
         this.title = config.title;
         this.config = config;
         this.heroes = heroService.getHeroes();
+        this.heroImageUrl = 'dev.wister.biz:4200/hero.jpg';
+        // app.use(express.static(__dirname + '/public'));
+        this.heroImageUrl = '/assets/images/hero.jpg';
+        this.evilTitle = 'Template <script>alert("evil never sleeps")</script>Syntax';
 
     }
 }
